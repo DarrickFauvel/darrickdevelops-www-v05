b3b097c7a11d5631e3451822110da39e80cc3dd9
Darrick Fauvel <76695983+DarrickFauvel@users.noreply.github.com>
2025-05-17 17:45:18 +0000

    Comment out sections

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import ButtonBackToTop from "../components/button-back-to-top";
 import ClientsSection from "../components/sections/clients";
 import ContactSection from "../components/sections/contact";
 import Footer from "../components/footer";
-import HeaderNav from "../components/header-nav";
+import HeaderNav from "../components/header/header-nav";
 import HeroSection from "../components/sections/hero";
 import KeepUpToDateSection from "../components/sections/keep-up-to-date";
 import MapSection from "../components/sections/map";
@@ -16,22 +16,7 @@ import { brand } from "../constants/config";
 
 export default function Home() {
   return (
-    <div id="main" className="relative">
-      <ButtonBackToTop />
-
-      <header className="w-full z-50 top-0 py-3 sm:py-5 absolute">
-        <div className="container flex items-center justify-between">
-          <div>
-            <a className="flex items-center gap-2 text-white" href="/">
-              <span className="text-2xl">&lt;/&gt;</span>
-              <span className="leading-4 text-xl">{brand.name}</span>
-            </a>
-          </div>
-
-          <HeaderNav />
-        </div>
-      </header>
-
+    <>
       <main>
         <HeroSection />
 
@@ -48,7 +33,6 @@ export default function Home() {
         {/* <StatisticsSection /> */}
 
         {/* <BlogSection /> */}
-
         <ContactSection />
 
         {/* <MapSection /> */}
@@ -57,6 +41,6 @@ export default function Home() {
       </main>
 
       <Footer />
-    </div>
+    </>
   );
 }
